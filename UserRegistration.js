263673a1e9cac1469efcc0406e4c262cac718277
saurabh chopade <saurabhchopade3250@gmail.com>
2020-08-03 10:05:25 +0530

    ADDED : Code to validate firstName

--- UserRegistration.js
+++ UserRegistration.js
@@ -1,1 +1,13 @@
-var message = "Welcome";
+///Packege
+const prompt = require('prompt-sync')();
+//Regx patterns
+const firstNameRegx = "^[A-Z]{1}[a-z]{2,}$";
+var  firstName = prompt('Enter your firstname');
+	if(firstName.match(firstNameRegx))
+	{
+		console.log("Valid firstName");
+	}
+	else{
+		console.log("Invalid lastName");
+	}
+
